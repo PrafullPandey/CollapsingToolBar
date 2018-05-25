4711b655d63680240a74dde72f6e3f255a29205c
PrafullPandey <p2.prafull@gmail.com>
2018-05-25 06:49:39 +0530

    downloaded images stored locally

--- app/src/main/java/com/corific/p2_vaio/corific_ui/activity/MainActivity.java
+++ app/src/main/java/com/corific/p2_vaio/corific_ui/activity/MainActivity.java
@@ -1,14 +1,21 @@
 package com.corific.p2_vaio.corific_ui.activity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +25,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.corific.p2_vaio.corific_ui.R;
 import com.corific.p2_vaio.corific_ui.adapter.ViewPagerAdapter;
@@ -36,15 +44,17 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int WRITE_EXTERNAL_STORAGE = 1;
+    public static final int READ_EXTERNAL_STORAGE = 2;
     private static final String TAG = "MainActivity";
     private float collapsedScale;
     private float expandedScale;
-    private ImageView photoView ;
+    private ImageView photoView;
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private ViewPagerAdapter adapter;
-    private String[] urls ;
-    private int imageIndex=0;
+    private String[] urls;
+    private int imageIndex = 0;
     private ImageView image;
 
     private static void scalePhotoImage(ImageView photoView, float scale) {
@@ -61,17 +71,39 @@ public class MainActivity extends AppCompatActivity {
         float centerX = photoView.getWidth() / 2F;
         float centerY = photoView.getHeight() / 2F;
 
-        Log.d(TAG, "scalePhotoImage: \n"+"offsetX => "+offsetX
-                                             +"\noffsetY => "+offsetY
-                                            +"\ncenterY => "+centerX
-                                            +"\ncenterY => "+centerY
-                                            +"\nScale => "+scale);
+        /*Log.d(TAG, "scalePhotoImage: \n" + "offsetX => " + offsetX
+                + "\noffsetY => " + offsetY
+                + "\ncenterY => " + centerX
+                + "\ncenterY => " + centerY
+                + "\nScale => " + scale);*/
 
         Matrix imageMatrix = new Matrix();
         imageMatrix.setScale(scale, scale, centerX, centerY);
         imageMatrix.preTranslate(offsetX, offsetY);
 
         photoView.setImageMatrix(imageMatrix);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "onActivityResult: WRITE_EXTERNAL_STORAGE "+imageIndex);
+                    GetImageFromURL getImageFromURL = new GetImageFromURL();
+                    getImageFromURL.execute(urls[imageIndex]);
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+
+        }
     }
 
     @Override
@@ -89,28 +121,24 @@ public class MainActivity extends AppCompatActivity {
 
         photoView = (ImageView) findViewById(R.id.image);
 
-        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
-        viewPager = (ViewPager)findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
 
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabTextColors(getResources().getColor(R.color.colorAccent), Color.parseColor("#ffffff"));
 //        createTabIcons();
 
-        NestedScrollView scrollView = (NestedScrollView) findViewById (R.id.nest_scrollView);
-        scrollView.setFillViewport (true);
-
+        NestedScrollView scrollView = (NestedScrollView) findViewById(R.id.nest_scrollView);
+        scrollView.setFillViewport(true);
 
         urls = getResources().getStringArray(R.array.image_URL);
-        image = (ImageView)findViewById(R.id.image);
+        image = (ImageView) findViewById(R.id.image);
 
         Random random = new Random();
         imageIndex = random.nextInt(3);
 
-        GetImageFromURL getImageFromURL = new GetImageFromURL();
-        getImageFromURL.execute(urls[imageIndex]);
-
-
+        getPermissions();
 
         AppBarLayout mAppBarLayout = (AppBarLayout) findViewById(R.id.appBar);
         mAppBarLayout.setExpanded(false);
@@ -128,17 +156,17 @@ public class MainActivity extends AppCompatActivity {
                     int bitmapWidth = photo.getIntrinsicWidth();
                     int bitmapHeight = photo.getIntrinsicHeight();
 
-                    collapsedScale = (float)photoView.getWidth()/(float)bitmapWidth;
-                    expandedScale = (float)photoView.getHeight()/(float)bitmapHeight;
+                    collapsedScale = (float) photoView.getWidth() / (float) bitmapWidth;
+                    expandedScale = (float) photoView.getHeight() / (float) bitmapHeight;
 
                     scalePhotoImage(photoView, expandedScale);
-                    Log.d(TAG, "onOffsetChanged: if\nscrollPercent => "+scrollPercent+"\n"+collapsedScale+"\n"+expandedScale);
+//                    Log.d(TAG, "onOffsetChanged: if\nscrollPercent => " + scrollPercent + "\n" + collapsedScale + "\n" + expandedScale);
 
 
                 } else {
 
-                    scalePhotoImage(photoView, (collapsedScale + (expandedScale - collapsedScale) * (scrollPercent))/2);
-                    Log.d(TAG, "onOffsetChanged: else\nscrollPercent => "+scrollPercent+"\n"+collapsedScale+"\n"+expandedScale);
+                    scalePhotoImage(photoView, (collapsedScale + (expandedScale - collapsedScale) * (scrollPercent)) / 2);
+//                    Log.d(TAG, "onOffsetChanged: else\nscrollPercent => " + scrollPercent + "\n" + collapsedScale + "\n" + expandedScale);
 
                 }
 
@@ -148,43 +176,39 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    private void getPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            GetImageFromURL getImageFromURL = new GetImageFromURL();
+            getImageFromURL.execute(urls[imageIndex]);
 
-            //setting up viewPager for swipe view between ALL/MY Listings
-            adapter = new ViewPagerAdapter(getSupportFragmentManager());
-            adapter.addFragment(new Reviews(), "Reviews");
-            adapter.addFragment(new Qualification(), "Qualification");
-            adapter.addFragment(new More(), "More");
-            viewPager.setAdapter(adapter);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE);
         }
 
-    private void createTabIcons() {
-
-        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabOne.setText("Reviews");
-//        tabOne.setBackground(getResources().getDrawable(R.drawable.rounded_rect_bg));
-//        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.analytics, 0, 0);
-        tabLayout.getTabAt(0).setCustomView(tabOne);
-
-        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabTwo.setText("Qualification");
-//        tabOne.setBackground(getResources().getDrawable(R.drawable.rounded_rect_bg));
-        tabLayout.getTabAt(1).setCustomView(tabTwo);
-
-        TextView tabThree = (TextView) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
-        tabThree.setText("More");
-//        tabOne.setBackground(getResources().getDrawable(R.drawable.rounded_rect_bg));
-        tabLayout.getTabAt(2).setCustomView(tabThree);
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+             } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE);
+        }
     }
 
-    class GetImageFromURL extends AsyncTask<String ,Void ,Void>{
+    private void setupViewPager(ViewPager viewPager) {
+
+        //setting up viewPager for swipe view between ALL/MY Listings
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Reviews(), "Reviews");
+        adapter.addFragment(new Qualification(), "Qualification");
+        adapter.addFragment(new More(), "More");
+        viewPager.setAdapter(adapter);
+    }
+
+    class GetImageFromURL extends AsyncTask<String, Void, Void> {
 
         private ImageView image_user;
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            image_user = (ImageView)findViewById(R.id.image);
+            image_user = (ImageView) findViewById(R.id.image);
         }
 
         @Override
@@ -203,9 +227,8 @@ public class MainActivity extends AppCompatActivity {
                 connection.connect();
                 InputStream input = connection.getInputStream();
                 Bitmap myBitmap = BitmapFactory.decodeStream(input);
-                myBitmap = Bitmap.createScaledBitmap(myBitmap ,200 ,200 ,false);
+                myBitmap = Bitmap.createScaledBitmap(myBitmap, 200, 200, false);
                 final Bitmap newBitmap = myBitmap;
-                saveImageOnDevice(newBitmap);
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -215,6 +238,8 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+                saveImageOnDevice(newBitmap);
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -223,27 +248,28 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        private void saveImageOnDevice(Bitmap newBitmap) {
-
+        private void saveImageOnDevice(Bitmap bitmap) {
+            Log.d(TAG, "saveImageOnDevice: In");
             try {
-                String root = Environment.getExternalStorageDirectory().toString();
-                File myDir = new File(root + "/DCIM");
+                FileOutputStream outStream = null;
+                File sdCard = Environment.getExternalStorageDirectory();
+                File dir = new File(sdCard.getAbsolutePath() + "/TestImage");
+                dir.mkdirs();
+                String fileName = "USER_IMAGE.jpg";
+                File outFile = new File(dir, fileName);
+                outStream = new FileOutputStream(outFile);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                outStream.flush();
+                outStream.close();
 
-                if (!myDir.exists()) {
-                    myDir.mkdirs();
-                }
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                intent.setData(Uri.fromFile(outFile));
+                sendBroadcast(intent);
+                Toast.makeText(getApplicationContext() , "Image Saved Succesfully" ,Toast.LENGTH_SHORT).show();
 
-                String name = "UserImage.jpg";
-                myDir = new File(myDir, name);
-                FileOutputStream out = new FileOutputStream(myDir);
-                newBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            } catch (Exception e) {
+                Log.e("saveToInternalStorage()", e.getMessage());
 
-                out.flush();
-                out.close();
-
-            } catch(Exception e){
-                // some action
-                e.printStackTrace();
             }
         }
     }
